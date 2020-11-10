54bbcdc445d59fd17171bc335ba08fa6f06caced
avlambertucci <armandolambertucci@gmail.com>
2020-11-10 16:54:16 -0300

    form action to cotacao

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const convert = require('./lib/convert')
+
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) =>{
   res.render('home')
+})
+app.get('/cotacao', (req, res) =>{
+  const {cotacao, quantidade} = req.query
+  const conversao = cotacao*quantidade
+  res.render('cotacao', {
+    cotacao,
+    quantidade,
+    conversao
+  })
 })
 
 app.listen(3000, ()=> console.log('conectado!'))
